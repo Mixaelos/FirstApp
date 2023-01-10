976bc74469a2830e5c2f9ed48ff76af672d2ddaf
Михаил Тепляков <Fireball96@yandex.ru>
2023-01-10 03:16:30 +0300

    Добавлен файл Program с разными типами переменных.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("как вас зовут?");
+            string Name = Console.ReadLine();
+            Console.WriteLine("сколько вам лет?");
+            byte age = byte.Parse(Console.ReadLine());
+            Console.WriteLine("У вас есть питомец? (true or false)");
+            bool pet = bool.Parse(Console.ReadLine());
+            Console.WriteLine("какой у вас размер ноги?");
+            double size = double.Parse(Console.ReadLine());
+            Console.WriteLine($"как вас зовут?\t {Name}.\nсколько вам лет?\t {age}. \nУ вас есть питомец?\t {pet}. \nкакой у вас размер ноги?\t {size}.");
+
+        }
+    }
+}
